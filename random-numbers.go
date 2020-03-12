babeb06edf840dff9701251e7d8508e12ce6681c
lhp9916 <lhp9916@gmail.com>
2020-03-12 21:35:45 +0800

    随机数

---
+++ random-numbers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	p := fmt.Println
+
+	p(rand.Int())
+	p(rand.Intn(100))
+
+	p(rand.Float64())
+	p(rand.Float64()*5 + 5)
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	fmt.Print(r1.Intn(100), ",")
+	fmt.Print(r1.Intn(100))
+	fmt.Println()
+
+	s2 := rand.NewSource(42)
+	r2 := rand.New(s2)
+	fmt.Print(r2.Intn(100), ",")
+	fmt.Print(r2.Intn(100))
+	fmt.Println()
+	s3 := rand.NewSource(42)
+	r3 := rand.New(s3)
+	fmt.Print(r3.Intn(100), ",")
+	fmt.Print(r3.Intn(100))
+}
