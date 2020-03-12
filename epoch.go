af716fd3df9bda6fa388bbe3ec1e0edfcb7f853b
lhp9916 <lhp9916@gmail.com>
2020-03-12 21:17:27 +0800

    时间

---
+++ epoch.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	secs := now.Unix()
+	nanos := now.UnixNano()
+	fmt.Println(now)
+
+	fmt.Println(secs)
+	fmt.Println(nanos)
+
+	fmt.Println(time.Unix(secs, 0))
+	fmt.Println(time.Unix(0, nanos))
+}
---
+++ time-formatting-parsing.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	p := fmt.Println
+
+	t := time.Now()
+	p(t.Format(time.RFC3339))
+
+	t1, _ := time.Parse(
+		time.RFC3339,
+		"2012-11-01T22:08:41+00:00")
+	p(t1)
+
+	p(t.Format("3:04PM"))
+	p(t.Format("Mon Jan _2 15:04:05 2006"))
+	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+	form := "3 04 PM"
+	t2, _ := time.Parse(form, "8 41 PM")
+	p(t2)
+
+	// 对于纯数字表示的时间，你也可以使用标准的格式化字
+	// 符串来提出时间值的组成。
+	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
+
+	// `Parse` 函数在输入的时间格式不正确时会返回一个错误。
+	ansic := "Mon Jan _2 15:04:05 2006"
+	_, e := time.Parse(ansic, "8:41PM")
+	p(e)
+}
